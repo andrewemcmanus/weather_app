2a02d220c9fc0adab99dde69bc5b4c19fb3c8c66
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-03-28 09:17:43 -0400

    add weather

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+# from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# SECRET_KEY = os.getenv("SECRET_KEY");
 
 
 # Quick-start development settings - unsuitable for production
@@ -36,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weather',
 ]
 
 MIDDLEWARE = [
