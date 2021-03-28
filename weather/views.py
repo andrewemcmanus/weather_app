60cdcaa8d226f10075a103e935c164fb637aef3e
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-03-28 11:07:24 -0400

    [weatherAPI] add key, .gitignore

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+import os
+from dotenv import load_dotenv
+import requests
+weather = os.path.expanduser('./weather')
+load_dotenv(os.path.join(weather, 'keys.py'))
+OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
 
 # Create your views here.
 def index(request):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid={OPENWEATHER_API_KEY}"
+    city = 'Las Vegas'
+    city_weather = requests.get(url.format(city)).json()
     return render(request, 'weather/index.html')
