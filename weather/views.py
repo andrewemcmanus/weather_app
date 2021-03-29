dd903891ed92f8d673a063efd8efd90304bb37bc
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-03-29 08:35:29 -0400

    [views] create json weather

--- weather/views.py
+++ weather/views.py
@@ -12,4 +12,12 @@ def index(request):
     url = f"http://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid={OPENWEATHER_API_KEY}"
     city = 'Las Vegas'
     city_weather = requests.get(url.format(city)).json()
+    # print(city_weather)
+    weather = {
+        'city': city,
+        'temperature': city_weather['main']['temp'],
+        'description': city_weather['weather'][0]['description'],
+        'icon': city_weather['weather'][0]['icon']
+    }
+    # print(weather)
     return render(request, 'weather/index.html')
