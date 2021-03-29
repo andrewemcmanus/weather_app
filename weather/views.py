41201744d7cda10523040b591267c5c8550d260e
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-03-29 09:25:33 -0400

    [models, admin] add cities

--- weather/views.py
+++ weather/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import os
 from dotenv import load_dotenv
 import requests
+from .models import City
 weather = os.path.expanduser('./weather')
 load_dotenv(os.path.join(weather, 'keys.py'))
 OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
@@ -10,14 +11,17 @@ OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
 # Create your views here.
 def index(request):
     url = f"http://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid={OPENWEATHER_API_KEY}"
-    city = 'Las Vegas'
-    city_weather = requests.get(url.format(city)).json()
-    # print(city_weather)
-    weather = {
-        'city': city,
-        'temperature': city_weather['main']['temp'],
-        'description': city_weather['weather'][0]['description'],
-        'icon': city_weather['weather'][0]['icon']
-    }
+    cities = City.objects.all()
+    weather_data = []
+    for city in cities:
+        city_weather = requests.get(url.format(city)).json()
+        weather = {
+            'city': city,
+            'temperature': city_weather['main']['temp'],
+            'description': city_weather['weather'][0]['description'],
+            'icon': city_weather['weather'][0]['icon']
+            }
+    weather_data.append(weather)
     # print(weather)
-    return render(request, 'weather/index.html')
+    context = {'weather_data': weather_data}
+    return render(request, 'weather/index.html', context)
